dd678e0c39ded0a210f9c068ad44b7bc008cfdcc
kimj0851 <50212368+kimj0851@users.noreply.github.com>
2020-05-07 12:07:29 -0700

    frq

---
+++ APSCA-2020/src/FRQ/OnlinePurchaseManager.java
@@ -0,0 +1,108 @@
+package FRQ;
+import java.util.ArrayList;
+
+public class OnlinePurchaseManager
+{
+   //purchases stores Gizmos
+   private ArrayList<Gizmo> purchases;
+   
+   
+   //point purchases at a new ArrayList   
+   public OnlinePurchaseManager()
+   {
+   	purchases = new ArrayList<Gizmo>();
+   }
+   
+   
+   //NOT part of the sample question
+   //needed in order to type up and
+   //test student code
+   public void add( String m, boolean e )
+   {
+   	purchases.add( new Gizmo( m, e) );
+   }
+   
+   
+   //Part A
+   //count the number of Gizmos made by maker
+   //that are also electronic
+   //must call getMaker and isElectronic
+   //return the count or 0 if no matches
+   public int countElectronicsByMaker(String maker)
+   {   
+	   int counter =0;
+   	//WRITE CODE HERE FOR PART A
+	   for (int i = 0; i < purchases.size(); i++)
+	   {
+
+		   if (purchases.get(i).getMaker() == maker)
+		   {
+			   if (purchases.get(i).isElectronic())
+			   {
+				   counter ++;
+			   }
+		   }
+	   }
+   	return counter;
+   }
+   
+
+   //Part B
+   //loop thought the list and check for matching adjacent pairs
+   //return true of any adjacent [ side by side ] pairs are the same
+   //to be the same the pairs must have the same maker and
+   //be either electronic or non-electronic
+   //you really really should use the equals method from Gizmo
+   public boolean hasAdjacentEqualPair()
+   {  
+	   boolean adj = false;
+	   for (int i = 0; i < purchases.size()-1; i++)
+	   {
+		   if (purchases.get(i).equals(purchases.get(i+1)))
+		   {
+			  adj = true;
+		   }
+	   }
+   	//WRITE CODE HERE FOR PART B
+	   
+   	return adj;   	
+   }
+   
+   
+   //NOT part of the sample question
+   //needed in order to type up and
+   //test student code
+   public String toString()
+   {
+   	return "" + purchases;
+   }
+}
+
+
+/*Part C
+
+OPEN-ENDED SECTION
+
+A programmer would like to add a method getCheapestGizmoByMaker, 
+which returns the least expensive Gizmo purchased by an individual 
+from a given maker.
+
+Write a description of how you would change the Gizmo and OnlinePurchaseManager 
+classes in order to support this modification. 
+ 
+Make sure to include the following in your response. 
+
+•	Write the method header for the getCheapestGizmoByMaker method. 
+	DO write code for this part.
+
+•	Identify any new or modified variables, constants, constructors, 
+   or methods aside from the getCheapestGizmoByMaker method. 
+   DO NOT write code for this part.
+   
+•	Describe, for each new or revised variable, constant, constructor, 
+   or method, how it would change or be implemented, including visibility and type. 
+   You do not need to describe the getCheapestGizmoByMaker method.
+   DO NOT write code for this part.
+
+*/
+
---
+++ APSCA-2020/src/winter/SnowMan.java
@@ -0,0 +1,37 @@
+package winter;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+public class SnowMan extends AbstractShape
+{
+   public SnowMan(int x, int y, int w, int h )
+   {
+		super(x, y, w, h, Color.WHITE, 0, 0);
+   }
+
+   public void draw(Graphics window)
+   {
+      //add code here to make a snowman 
+	   window.setColor(Color.white);
+		
+		window.fillOval(getXPos() , getYPos(), getWidth(), getHeight());
+		window.fillOval((int) (getXPos() + getWidth()* .7 / 4), getYPos() - 50, (int) (getWidth() * .7), (int) (getHeight()  *.7));
+		window.fillOval((int) (getXPos() + getWidth() * .5  / 2) , getYPos() - 85, (int) (getWidth() * .5), (int) (getHeight()  *.5));
+   }
+
+   public void moveAndDraw(Graphics window)
+   {
+      draw(window);   
+	   setXPos(getXPos()+getXSpeed());
+	   setYPos(getYPos()+getYSpeed());
+	      
+	   window.setColor(getColor());
+	      
+	   draw(window);
+   }
+}
